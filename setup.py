4b2f46dc74b7eacf6a95accd1d846756afd7d271
Lux <sr.lux1nt@gmail.com>
2022-04-24 10:05:32 +0300

    [build] Use setuptools instead of distutils.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usb/bin/env python3
 """Setup script."""
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Polydating bot',
@@ -10,10 +10,5 @@ setup(
     author='Lux',
     author_email='sr.Lux1nt@gmail.com',
     url='https://github.com/LuxInTenebr1s/polydating_bot',
-    packages=[
-        'polydating_bot',
-        'polydating_bot.data',
-        'polydating_bot.store',
-        'polydating_bot.handlers'
-    ],
+    packages=find_packages(),
 )
